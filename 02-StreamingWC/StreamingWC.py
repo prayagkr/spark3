4eed5ceaa1fadf739bf581e32b32030a15ebcbb2
Pramod Kumar Sharma <prayagkr@gmail.com>
2021-01-19 01:40:38 +0530

    basic streaming complete

---
+++ 02-StreamingWC/StreamingWC.py
@@ -0,0 +1,36 @@
+import sys
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import expr
+from lib.logger import Log4j
+from lib.utils import get_spark_app_config
+
+if __name__ == '__main__':
+    conf = get_spark_app_config()
+    # spark = SparkSession.builder.appName('Hello Spark').master('local[3]').getOrCreate()
+    spark: SparkSession = SparkSession.builder \
+        .config(conf=conf) \
+        .getOrCreate()
+    # # To check configuration
+    # conf_out = spark.sparkContext.getConf()
+    # print(conf_out.toDebugString())
+
+    logger = Log4j(spark)
+
+    lines_df = spark.readStream\
+        .format("socket") \
+        .option("host", "localhost") \
+        .option("port", "9999") \
+        .load()
+
+    # lines_df.printSchema()
+    words_df = lines_df.select(expr("explode(split(value, ' ')) as word"))
+    counts_df = words_df.groupBy("word").count()
+
+    word_count_query = counts_df.writeStream \
+        .format("console") \
+        .option("checkpointLocation", "chk-point-dir") \
+        .outputMode("complete") \
+        .start()
+
+    word_count_query.awaitTermination()
